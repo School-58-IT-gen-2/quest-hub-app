b5221bf9a76a000942bf9c2cc7bee9b4b9fa3764
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-11-14 21:52:13 +0300

    ТЕСТЫ РАБОТАЮТ!1!1!!!1!

--- adapters/tests/supabase_db_adapter.py
+++ adapters/tests/supabase_db_adapter.py
@@ -1,22 +1,32 @@
 from supabase import create_client, Client
 from datetime import datetime, timezone
 import supabase
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class SupabaseDBAdapter :
     def __init__(self, url:str, key:str) -> None:
-        supabase: Client = create_client(url, key)
+        self.supabase: Client = create_client(url, key)
 
-    def get_user_data_by_id(table:str, id:int) -> list:
-        user_data = supabase.table(table).select("*").eq("id", id).execute()
+    def get_user_data_by_id(self, table:str, id:int) -> list:
+        user_data = self.supabase.table(table).select("*").eq("id", id).execute()
         return user_data.data
     
-    def create_new_user(table:str, id:int) -> None:
+    def create_new_user(self, table:str, id:int) -> None:
         new_user = {
             "id": id,
-            "created_at": str(datetime.now(timezone.utc))
+            "created_at": str(datetime.now(timezone.utc)),
+            "test": "test supabase",
+            "test2": 123
         }
 
-        supabase.table(table).insert(new_user).execute()
+        self.supabase.table(table).insert(new_user).execute()
 
-    def update_user(table:str, id:int) -> None:
-        supabase.table(table).update({"created_at": str(datetime.now(timezone.utc))}).eq("id", id).execute()
+    def update_user(self, table:str, id:int) -> None:
+        self.supabase.table(table).update({"created_at": str(datetime.now(timezone.utc))}).eq("id", id).execute()
+
+    def get_all_users_data(self, table:str) -> list:
+        user_data = self.supabase.table(table).select("*").execute()
+        return user_data.data
+
--- adapters/tests/supabasetest.py
+++ adapters/tests/supabasetest.py
@@ -2,15 +2,17 @@ import unittest
 import os
 from supabase_db_adapter import *
 from dotenv import load_dotenv
+import random
+import time
 load_dotenv()
 
 class TestSupabaseAdapter(unittest.TestCase):
     def test_create_user(self):
         try:
             adapter = SupabaseDBAdapter(url=os.getenv("SUPABASE_URL"), key=os.getenv("SUPABASE_KEY")) 
-            print("Successfully created adapter :)\n")
-            adapter.create_new_user("test", 1)
-            print("Successfully created users in table :)\n")
+            #print("Successfully created adapter :)\n")
+            adapter.create_new_user("test",int(time.time()))
+            #print("Successfully created users in table :)\n")
         except:
             self.fail("Failed to create users in table :(((")
 
@@ -18,9 +20,10 @@ class TestSupabaseAdapter(unittest.TestCase):
     def test_get_user_data(self):
         try:
             adapter = SupabaseDBAdapter(url=os.getenv("SUPABASE_URL"), key=os.getenv("SUPABASE_KEY"))
-            print("Successfully created adapter :)")
+            #print("Successfully created adapter :)")
             user_data = adapter.get_user_data_by_id("test", 1)
-            print("Successfully retrieved user data :)")
+            print(user_data)
+            self.assertIsNotNone(user_data)
         except:
             self.fail("Failed to retrieve user data :(((")
 
@@ -28,11 +31,13 @@ class TestSupabaseAdapter(unittest.TestCase):
     def test_update_user(self):
         try:
             adapter = SupabaseDBAdapter(url=os.getenv("SUPABASE_URL"), key=os.getenv("SUPABASE_KEY"))
-            print("Successfully created adapter :)")
-            adapter.update_user("test", 1)
-            print("Successfully updated user data :)")
+            user_data_old = adapter.get_user_data_by_id("test",1)
+            adapter.update_user("test",1)
+            user_data_new = adapter.get_user_data_by_id("test",1)
+            self.assertNotEqual(user_data_old, user_data_new)
         except:
             self.fail("Failed to update user data :(((")
+
 
 
 
