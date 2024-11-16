1f1903a827538aa6cc5c6214bf698d9ea0a8ea3b
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-11-16 21:21:31 +0300

    перенес тесты в ветку, склонированную с dev, но пока не работает, тк не может найти модуль с адаптером

---
+++ fastapi_example/tests/supabasetest.py
@@ -0,0 +1,46 @@
+import unittest
+import os
+from fastapi_example.fastapi_example.adapters.supabase import *
+from dotenv import load_dotenv
+import random
+import time
+from datetime import datetime,timezone
+load_dotenv()
+
+class TestSupabaseAdapter(unittest.TestCase):
+    def test_select_user(self):
+        try:
+            supa = get_supabase_client()
+            self.assertIsNotNone(supa.table("test").select("*").eq("id",1).execute())
+        except:
+            self.fail("Failed to select user :(")
+
+    def test_create_user(self):
+        try:
+            supa = get_supabase_client()
+            new_user = {    
+                "id": int(time.time()),
+                "created_at": str(datetime.now(timezone.utc)),
+                "test": "test",
+                "test2": 123
+            }
+            supa.table("test").insert(new_user).execute()
+        except:
+            self.fail("Failed to create user :(")   
+
+    def test_update_user(self):
+        try:
+            supa = get_supabase_client()
+            user_old = supa.table("test").select("*").eq("id",1).execute()
+            supa.table("test").update({"created_at": str(datetime.now(timezone.utc))}).eq("id", id).execute()
+            user_new = supa.table("test").select("*").eq("id",1).execute()
+            self.assertNotEqual(user_old,user_new)
+        except:
+            self.fail("Failed to update user info :(")
+
+
+
+
+if __name__ == '__main__':
+        unittest.main()
+
