b26ece882e9bafb7438e9162634510919d990a6e
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-11-20 18:06:21 +0300

    переписал тесты под db_source, но все еще не работают импорты

--- fastapi_example/tests/supabasetest.py
+++ fastapi_example/tests/supabasetest.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from fastapi_example.fastapi_example.adapters.supabase import *
+from adapters.db_source import *
 from dotenv import load_dotenv
 import time
 from datetime import datetime,timezone
@@ -9,33 +9,36 @@ load_dotenv()
 class TestSupabaseAdapter(unittest.TestCase):
     def test_select_user(self):
         try:
-            supa = get_supabase_client()
-            self.assertIsNotNone(supa.table("test").select("*").eq("id",1).execute())
+            supa = DBSource(os.getenv("SUPABASE_URL"),os.getenv("SUPABASE_KEY"))
+            supa.connect()
+            self.assertIsNotNone(supa.get_all("test"))
         except:
             self.fail("Failed to select user :(")
 
     def test_create_user(self):
         try:
-            supa = get_supabase_client()
-            len1 = len(supa.table("test").select("*").execute())
+            supa = DBSource(os.getenv("SUPABASE_URL"),os.getenv("SUPABASE_KEY"))
+            supa.connect()
+            len1 = len(supa.get_all())
             new_user = {    
                 "id": int(time.time()),
                 "created_at": str(datetime.now(timezone.utc)),
                 "test": "test",
                 "test2": 123
             }
-            supa.table("test").insert(new_user).execute()
-            len2 = len(supa.table("test").select("*").execute())
+            supa.insert("test",new_user)
+            len2 = len(supa.get_all("test"))
             self.assertNotEqual(len1,len2)
         except:
             self.fail("Failed to create user :(")   
 
     def test_update_user(self):
         try:
-            supa = get_supabase_client()
-            user_old = supa.table("test").select("*").eq("id",1).execute()
-            supa.table("test").update({"created_at": str(datetime.now(timezone.utc))}).eq("id", id).execute()
-            user_new = supa.table("test").select("*").eq("id",1).execute()
+            supa = DBSource(os.getenv("SUPABASE_URL"),os.getenv("SUPABASE_KEY"))
+            supa.connect()
+            user_old = supa.get_by_id("test",1)
+            supa.update("test",{"created_at": str(datetime.now(timezone.utc))},1)
+            user_new = supa.get_by_id("test",1)
             self.assertNotEqual(user_old,user_new)
         except:
             self.fail("Failed to update user info :(")
