71767d6d288fbef3a22eecd66f0f25c856ebc13f
DukeTime <duke.of.times@gmail.com>
2024-11-27 19:40:33 +0300

    sing-up

--- fastapi_example/fastapi_example/app/routes/v1/auth.py
+++ fastapi_example/fastapi_example/app/routes/v1/auth.py
@@ -2,34 +2,28 @@ from fastapi import APIRouter
 from fastapi_example.modules.user.models import UserModel
 from fastapi_example.adapters.supabase import get_supabase_client
 from ......model.user_model import User
+from ......adapters.db_source import DBSource
+from ......net_config import settings
 
 
 route = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @route.post(path="/sign-up")
-def sign_up(user: UserModel):
+def sign_up(tg_id: int, first_name: str):
     try:
-        supa = get_supabase_client()
-        credentials = {
-            "email": user.email,
-            "password": user.password.get_secret_value(),
-        }
-        supa_user = supa.auth.sign_up(credentials=credentials)
-        return supa_user
+        new_db_source = DBSource(settings.supabase.url, settings.supabase.key)        
+        new_user = User(tg_id, new_db_source, first_name)
+        new_user.insert()
+        return new_user.__dict__()
     except Exception as error:
         print(error)
 
 
-@route.post(path="/sign-in")
+@route.get(path="/sign-in")
 def sign_in(user: UserModel):
     try:
-        supa = get_supabase_client()
-        credentials = {
-            "email": user.email,
-            "password": user.password.get_secret_value(),
-        }
-        supa_user = supa.auth.sign_in_with_password(credentials=credentials)
+        
         return supa_user
     except Exception as error:
         print(error)
