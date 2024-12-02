c5e7e45dcb55210becd58172ad0014f1045d2e66
USER <tilzziter@gmail.com>
2024-12-02 21:06:27 +0300

    some refactoring ++++

--- quest_hub_fastapi_server/app/routes/v1/__init__.py
+++ quest_hub_fastapi_server/app/routes/v1/__init__.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-
 from quest_hub_fastapi_server.app.routes.v1.auth import route as auth_route
 
 route = APIRouter(prefix="/v1")
 
 route.include_router(auth_route)
+
 
