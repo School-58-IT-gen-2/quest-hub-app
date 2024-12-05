114a6d754387d746524a78b2b4f5d80c91d94c14
Sergey Smaglyuk <smegloy@gmail.com>
2024-12-05 22:06:39 +0300

    * fix query -> body

--- model/user_model.py
+++ model/user_model.py
@@ -6,11 +6,24 @@ from typing import List
 
 class User(AbstractModel):
 
-    def __init__(self, tg_id: int, db_source: DBSource, first_name: str, username: str = None, last_name: str = None, role: str = None, id: str = None, age: int = None, is_bot: bool = None, language_code: str = None, is_premium: bool = None) -> None:
-        """ 
+    def __init__(
+        self,
+        tg_id: str,
+        db_source: DBSource,
+        first_name: str,
+        username: str = None,
+        last_name: str = None,
+        role: str = None,
+        id: str = None,
+        age: int = None,
+        is_bot: bool = None,
+        language_code: str = None,
+        is_premium: bool = None,
+    ) -> None:
+        """
         :param int tg_id: id пользователя в Telegram
         :param DBSource db_source: Объект класса базы данных
-        :param str first_name: Имя 
+        :param str first_name: Имя
         :param str username: Имя пользователя в Telegram
         :param str last_name: Фамилия
         :param str role: Роль (игрок или мастер)
@@ -32,12 +45,12 @@ class User(AbstractModel):
         self.__language_code = language_code
         self.__is_premium = is_premium
         self.__char_lists = {}
-        self.table_name = 'profiles'
+        self.table_name = "profiles"
 
     def insert(self) -> dict:
         """
         Сохранение пользователя в базу данных
-        
+
         :return dict: Словарь с данными пользователя
         """
         data_list = self.get_by_value("tg_id", self.__tg_id)
@@ -47,7 +60,7 @@ class User(AbstractModel):
             self.__id = self.__db_source.insert(self.table_name, insert_dict)[0]["id"]
         self.synchronize()
         return self.__dict__()
-    
+
     def update(self, dict: dict) -> List[dict]:
         """
         Изменение данных о пользователе
@@ -56,19 +69,21 @@ class User(AbstractModel):
         :return List[dict]: Список из словаря с новой строкой
         """
         if self.__id == None:
-            raise Exception("id пользователя не может быть None. Добавьте пользователя в базу данных методом insert.")
+            raise Exception(
+                "id пользователя не может быть None. Добавьте пользователя в базу данных методом insert."
+            )
         data_list = self.__db_source.update(self.table_name, dict, self.__id)
         self.synchronize()
         return data_list
-    
+
     def delete(self) -> List[dict]:
         """
         Удаление пользователя из базы данных
-        
+
         :return List[dict]: Список из словаря с удалённой строкой
         """
         return self.__db_source.delete(self.table_name, self.__id)
-    
+
     def get_by_id(self, id: str) -> List[dict]:
         """
         Получение пользователя по id
@@ -86,8 +101,10 @@ class User(AbstractModel):
         :param str / int / list parameter_value: Значение, по которому происходит сравнение
         :return List[dict]: Список из словаря со строкой таблицы
         """
-        return self.__db_source.get_by_value(self.table_name, parameter, parameter_value)
-    
+        return self.__db_source.get_by_value(
+            self.table_name, parameter, parameter_value
+        )
+
     def synchronize(self) -> None:
         """Синхронизация объекта класса и данных в таблицах"""
         data_list = self.get_by_value("tg_id", self.__tg_id)
@@ -96,25 +113,99 @@ class User(AbstractModel):
         else:
             data_dict = data_list[0]
             self.set_attributes(data_dict)
-            for i in self.__db_source.get_by_value('character_list', "user_id", self.__id):
+            for i in self.__db_source.get_by_value(
+                "character_list", "user_id", self.__id
+            ):
                 char_list = CharacterList(self.__id, self.__db_source)
                 char_list.synchronize(i["id"])
                 self.__char_lists[char_list.id] = char_list
 
-    
-    def create_char_list(self,
-                            name: str = None, race: str = None, character_class: str = None, stats: dict = None, hp: int = None, alignment: str = None, skills: dict = None, weapons_and_equipment: dict = None, ability_saving_throws: int = None, death_saving_throws: int = None, attacks: dict = None, spells: dict = None, passive_perception: int = None, traits_and_abilities: dict = None, initiative: int = None, lvl: int = None, speed: int = None, backstory: str = None, experience: int = None, valuables: dict = None, diary: str = None, notes: str = None, languages: dict = None, npc_relations: dict = None, inspiration: int = None, interference: bool = None, ownership_bonus: int = None, advantages: bool = None, attribute_points: int = None, special_fours: dict = None, weaknesses: dict = None, damage: dict = None, stat_modifiers: dict = None) -> None:
+    def create_char_list(
+        self,
+        name: str = None,
+        race: str = None,
+        character_class: str = None,
+        stats: dict = None,
+        hp: int = None,
+        alignment: str = None,
+        skills: dict = None,
+        weapons_and_equipment: dict = None,
+        ability_saving_throws: int = None,
+        death_saving_throws: int = None,
+        attacks: dict = None,
+        spells: dict = None,
+        passive_perception: int = None,
+        traits_and_abilities: dict = None,
+        initiative: int = None,
+        lvl: int = None,
+        speed: int = None,
+        backstory: str = None,
+        experience: int = None,
+        valuables: dict = None,
+        diary: str = None,
+        notes: str = None,
+        languages: dict = None,
+        npc_relations: dict = None,
+        inspiration: int = None,
+        interference: bool = None,
+        ownership_bonus: int = None,
+        advantages: bool = None,
+        attribute_points: int = None,
+        special_fours: dict = None,
+        weaknesses: dict = None,
+        damage: dict = None,
+        stat_modifiers: dict = None,
+    ) -> None:
         """Создание листа персонажа пользователя"""
         if self.__id == None:
-            raise Exception("id пользователя не может быть None. Добавьте пользователя в базу данных методом insert.")
-        char_list = CharacterList(self.__id, self.__db_source, None,  name, race, character_class, stats, hp, alignment, skills, weapons_and_equipment, ability_saving_throws, death_saving_throws, attacks, spells, passive_perception, traits_and_abilities, initiative, lvl, speed, backstory, experience, valuables, diary, notes, languages, npc_relations, inspiration, interference, ownership_bonus, advantages, attribute_points, special_fours, weaknesses, damage, stat_modifiers)
+            raise Exception(
+                "id пользователя не может быть None. Добавьте пользователя в базу данных методом insert."
+            )
+        char_list = CharacterList(
+            self.__id,
+            self.__db_source,
+            None,
+            name,
+            race,
+            character_class,
+            stats,
+            hp,
+            alignment,
+            skills,
+            weapons_and_equipment,
+            ability_saving_throws,
+            death_saving_throws,
+            attacks,
+            spells,
+            passive_perception,
+            traits_and_abilities,
+            initiative,
+            lvl,
+            speed,
+            backstory,
+            experience,
+            valuables,
+            diary,
+            notes,
+            languages,
+            npc_relations,
+            inspiration,
+            interference,
+            ownership_bonus,
+            advantages,
+            attribute_points,
+            special_fours,
+            weaknesses,
+            damage,
+            stat_modifiers,
+        )
         char_list.insert()
         self.__char_lists[char_list.id] = char_list
 
     def get_char_lists(self) -> dict:
         """
         Получение листов персонажей пользователя
-        
+
         :return dict: Словарь с листами персонажей пользователя
         """
         return self.__char_lists
@@ -135,9 +226,9 @@ class User(AbstractModel):
             "is_premium": self.__is_premium,
             "username": self.__username,
             "age": self.__age,
-            "tg_id": self.__tg_id
+            "tg_id": self.__tg_id,
         }
-    
+
     def set_attributes(self, attr_dict: dict) -> None:
         """
         Установка параметров пользователя, заданных в словаре
@@ -145,4 +236,4 @@ class User(AbstractModel):
         :param dict attr_dict: Словарь с параметрами, которые нужно установить пользователю
         """
         for key in attr_dict:
-            setattr(self, '_' + self.__class__.__name__ + '__' + key, attr_dict[key])
+            setattr(self, "_" + self.__class__.__name__ + "__" + key, attr_dict[key])
--- quest_hub_fastapi_server/app/routes/v1/auth.py
+++ quest_hub_fastapi_server/app/routes/v1/auth.py
@@ -1,39 +1,43 @@
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 from fastapi import HTTPException
 from model.user_model import User
 from adapters.db_source import DBSource
 from quest_hub_fastapi_server.modules.settings import settings
-
+from model.character_list_model import CharacterList
+from typing import Optional
 
 route = APIRouter(prefix="/auth", tags=["auth"])
 
 
+class SignUpUserModel(BaseModel):
+    tg_id: str
+    first_name: str
+    role: str = "player"
+    is_bot: bool = False
+    username: Optional[str] = Field(default=None)
+    age: int = None
+    last_name: str = None
+    is_premium: bool = False
+    language_code: str = "rus"
+
+
 @route.post(path="/sign-up")
-def sign_up(
-    tg_id: int,
-    first_name: str,
-    role: str = "player",
-    is_bot: bool = False,
-    username: str = None,
-    age: int = None,
-    last_name: str = None,
-    is_premium: bool = False,
-    language_code: str = "rus",
-):
+def sign_up(sing_up_user_model: SignUpUserModel):
     try:
         new_db_source = DBSource(settings.supabase.url, settings.supabase.key)
         new_db_source.connect()
         new_user = User(
-            tg_id,
-            new_db_source,
-            first_name,
-            username,
-            last_name,
-            role,
-            age=age,
-            is_bot=is_bot,
-            language_code=language_code,
-            is_premium=is_premium,
+            tg_id=sing_up_user_model.tg_id,
+            db_source=new_db_source,
+            first_name=sing_up_user_model.first_name,
+            username=sing_up_user_model.username,
+            last_name=sing_up_user_model.last_name,
+            role=sing_up_user_model.role,
+            age=sing_up_user_model.age,
+            is_bot=sing_up_user_model.is_bot,
+            language_code=sing_up_user_model.language_code,
+            is_premium=sing_up_user_model.is_premium,
         )
         user = new_user.insert()
         if user:
