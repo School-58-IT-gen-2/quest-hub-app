c02e635570d449ff92fa9e05f8cc9a415c742cb5
Sergey Smaglyuk <smegloy@gmail.com>
2024-12-05 21:16:45 +0300

    * add sync-async example and dockerfile

--- quest_hub_fastapi_server/app/routes/v1/sync_async.py
+++ quest_hub_fastapi_server/app/routes/v1/sync_async.py
@@ -8,35 +8,38 @@ route = APIRouter(prefix="/sync-async", tags=["/sync-async"])
 
 
 def test_sync(title: str):
+    """Тестовая синхронная функция"""
     for i in range(10):
         print(f"{title} - {i}")
         sync_sleep(1)
 
 
-@route.get(path="/sync")
-def test_sync_point():
-    test_sync("test_sync_point")  # корретно
-
-
 async def test_async(title: str):
+    """Тестовая асинхронная функция"""
     for i in range(10):
         print(f"{title} - {i}")
         await async_sleep(1)
 
 
+@route.get(path="/sync")
+def test_sync_point():
+    test_sync("test_sync_point")
+
+
 @route.get(path="/async")
-async def test_async_point(background_tasks: BackgroundTasks):
+async def test_async_point():
     # корретно
     await test_async("test_async_point")
     # ошибка
     # test_sync("test_async_point")
 
-    # asyncio.gather - корретно
-    # task_1 = create_task(test_async("test_async_point"))
-    # await gather(task_1)
-
-    # run_in_threadpool - корретно
+    # run_in_threadpool
     # await run_in_threadpool(test_sync, "test_async_point")
 
-    # BackgroundTasks - корректно
+    # asyncio.gather
+    # profile_task = create_task(test_async("profile_task"))
+    # char_task = create_task(test_async("char_task"))
+    # await gather(profile_task, char_task)
+
+    # BackgroundTasks
     # background_tasks.add_task(test_async, "test_async_point")
