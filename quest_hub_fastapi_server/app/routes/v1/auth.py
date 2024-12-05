44fb443765d289f799a24c365c26c996fe6ff1dc
DukeTime <duke.of.times@gmail.com>
2024-12-05 20:39:10 +0300

    sing in

--- quest_hub_fastapi_server/app/routes/v1/auth.py
+++ quest_hub_fastapi_server/app/routes/v1/auth.py
@@ -33,7 +33,7 @@ def sign_up(
             raise HTTPException(status_code=503, 
                                 detail={"error": "Service Unavailable", 
     "message":"Запрашиваемый сервис или ресурс временно недоступен. Обратитесь к администратору."})
-    except Exception as error:
+    except:
         raise HTTPException(status_code=500, 
     detail={"error": "Internal Server Error", 
             "message":"Неизвестная ошибка на сервере. Обратитесь к администратору."})
@@ -44,9 +44,16 @@ def sign_in(tg_id: int, first_name: str):
         new_db_source = DBSource(settings.supabase.url, settings.supabase.key)        
         new_user = User(tg_id, new_db_source, first_name)
         new_user.insert()
-        return new_user
-    except Exception as error:
-        print(error)
+        if new_user:
+            return new_user
+        else: 
+            raise HTTPException(status_code=503, 
+                                detail={"error": "Service Unavailable", 
+    "message":"Запрашиваемый сервис или ресурс временно недоступен. Обратитесь к администратору."})
+    except:
+        raise HTTPException(status_code=500, 
+    detail={"error": "Internal Server Error", 
+            "message":"Неизвестная ошибка на сервере. Обратитесь к администратору."})
 
 
 @route.post(path="/sign-out")
