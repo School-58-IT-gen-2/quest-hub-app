fd394e76e58486fed62fc31a2e38affe1a49016a
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-12-08 21:40:01 +0300

    Убрал лишнее

--- src/quest_hub_fastapi_server/modules/settings.py
+++ src/quest_hub_fastapi_server/modules/settings.py
@@ -7,7 +7,6 @@ class EnvirementsSettings(BaseSettings):
     FASTAPI_PORT: int
     SUPABASE_URL: str
     SUPABASE_KEY: SecretStr
-    BOT_TOKEN : SecretStr
 
     class Config:
         env_file = ".env"
@@ -27,13 +26,10 @@ class Supabase(BaseSettings):
     url: str = envirements.SUPABASE_URL
     key: SecretStr = envirements.SUPABASE_KEY
 
-class TelegramBot(BaseSettings):
-    token: SecretStr = envirements.BOT_TOKEN
 
 class EnvSettings(BaseSettings):
     fastapi: Fastapi = Fastapi()
     supabase: Supabase = Supabase()
-    telegram_bot: TelegramBot = TelegramBot()
 
 
 settings = EnvSettings()
