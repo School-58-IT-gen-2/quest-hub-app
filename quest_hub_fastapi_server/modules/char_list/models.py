721ad9fd8d6ef691fb1b6568cde2d72e5c413c93
tuhlopuz1 <tuhlopuz1@gmail.com>
2025-01-13 19:53:25 +0300

    synchronized char-list adapter with RnD json

--- quest_hub_fastapi_server/modules/char_list/models.py
+++ quest_hub_fastapi_server/modules/char_list/models.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException
 from pydantic import BaseModel
-from typing import Optional, Dict, Any
+from typing import Optional, Dict, Any, List
 
 
 class CharListRequestModel(BaseModel):
@@ -16,28 +16,29 @@ class CharListRequestModel(BaseModel):
     speed: Optional[int] = None  # int8
     experience: Optional[int] = None  # int8
     ownership_bonus: Optional[int] = None  # int8
-    ability_saving_throws: Optional[int] = None  # int8
+    ability_saving_throws: Optional[Dict[str,Any]] = None  # json
     death_saving_throws: Optional[int] = None  # int8
     interference: Optional[bool] = None  # bool
     advantages: Optional[bool] = None  # bool
     weapons_and_equipment: Optional[Dict[str, Any]] = None  # json
     spells: Optional[Dict[str, Any]] = None  # json
     traits_and_abilities: Optional[Dict[str, Any]] = None  # json
-    languages: Optional[Dict[str, Any]] = None  # json
+    languages: Optional[List[Any]] = None  # json
     attack_and_damage_values: Optional[Dict[str, Any]] = None  # json
     special_features: Optional[Dict[str, Any]] = None  # json
     weaknesses: Optional[Dict[str, Any]] = None  # json
     npc_relations: Optional[Dict[str, Any]] = None  # json
     name: Optional[str] = None  # text
-    valuables: Optional[Dict[str, Any]] = None  # json
-    skills: Optional[Dict[str, Any]] = None  # json
+    valuables: Optional[Dict[str, Any]] = None  # json 
+    skills: Optional[List[Any]] = None  # json
     stat_modifiers: Optional[Dict[str, Any]] = None  # json
     stats: Optional[Dict[str, Any]] = None  # json
     user_id: Optional[str] = None  # text
     inspiration: bool = True  # bool
     surname: Optional[str] = None # text
-    inventory: Optional[Dict[str, Any]] = None # json
+    inventory: Optional[List[Any]] = None # json
     age: Optional[int] = None # int8
+    worldview: Optional[str] = None # text
 
 
 
