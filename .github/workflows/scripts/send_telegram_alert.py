bd8691cc1fe8df4777bef8cc451acd16de8dccf4
Finikfin <finikgrib@gmail.com>
2025-02-12 21:53:44 +0300

    теперь бот пишет в группу

--- .github/workflows/scripts/send_telegram_alert.py
+++ .github/workflows/scripts/send_telegram_alert.py
@@ -3,13 +3,31 @@ import asyncio
 import os
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID') 
+THREAD_ID = os.getenv('THREAD_ID')  
 MESSAGE_TEXT = os.getenv('MESSAGE_TEXT')
 
+if not all([TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, MESSAGE_TEXT]):
+    raise ValueError("Не все переменные окружения заданы!")
+
+# Инициализируем бота
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 async def send_alert():
-    await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=MESSAGE_TEXT)
+    try:
+        if THREAD_ID:
+            await bot.send_message(
+                chat_id=TELEGRAM_CHAT_ID,
+                text=MESSAGE_TEXT,
+                message_thread_id=int(THREAD_ID) 
+            )
+        else:
+            await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=MESSAGE_TEXT)
+        print("Сообщение успешно отправлено!")
+    except Exception as e:
+        print(f"Ошибка при отправке сообщения: {e}")
+    finally:
+        await bot.close()
 
-if __name__ == '__send_telegram_alert.py__':
+if __name__ == '__main__':
     asyncio.run(send_alert())
