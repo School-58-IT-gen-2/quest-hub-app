5d6fd4f0aca9c50c2d55e8332fd85d3433055f68
Step4ansky <stepa081022@gmail.com>
2025-02-20 16:57:44 +0300

    fix_send_alert_script

--- scripts/send_telegram_alert.py
+++ scripts/send_telegram_alert.py
@@ -2,6 +2,7 @@ from aiogram import Bot
 import asyncio
 import os
 from dotenv import load_dotenv
+from aiogram.exceptions import TelegramRetryAfter
 
 load_dotenv()
 
@@ -16,21 +17,26 @@ if not all([TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID, MESSAGE_TEXT]):
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
+
 async def send_alert():
     try:
         if THREAD_ID:
             await bot.send_message(
-                chat_id=TELEGRAM_CHAT_ID,
+                chat_id=int(TELEGRAM_CHAT_ID),
                 text=MESSAGE_TEXT,
                 message_thread_id=int(THREAD_ID) 
             )
         else:
             await bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=MESSAGE_TEXT)
         print("Сообщение успешно отправлено!")
+    except TelegramRetryAfter as e:
+        print(f"Превышен лимит запросов. Повторная попытка через {e.retry_after} секунд.")
+        await asyncio.sleep(e.retry_after)
+        await send_alert()  # Повторная попытка отправки
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
     finally:
-        await bot.close()
+        await bot.session.close()
 
 if __name__ == '__main__':
     asyncio.run(send_alert())
