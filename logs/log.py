4ffe2dc33d54b3dca3ec6206794ec72af2e31625
Step4ansky <stepa081022@gmail.com>
2025-04-03 21:59:21 +0300

    Лев разрещаишл пушить в мастер, тест локи на деплое

--- logs/log.py
+++ logs/log.py
@@ -4,6 +4,7 @@ import asyncio
 import sys
 import traceback
 from datetime import datetime, timezone, timedelta
+from logging_loki import LokiHandler
 
 # Настройка логирования
 log_file = "app_activity.log"
@@ -17,11 +18,37 @@ if not os.path.exists(log_file) or os.stat(log_file).st_size == 0:
             "----------------------------------------------------------------------------------------------\n"
         )
 
-logging.basicConfig(
+
+
+'''logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
     format="%(message)s",  # Отключаем стандартный формат, будем писать кастомные строки
 )
+'''
+
+# Создаем логгер
+logger = logging.getLogger("my_app")
+logger.setLevel(logging.DEBUG)  # Уровень логирования
+
+# --- 1. FileHandler (логи в файл) ---
+file_handler = logging.FileHandler("app_activity.log")
+file_handler.setLevel(logging.INFO)  # Только INFO и выше
+file_formatter = logging.Formatter("%(message)s")
+file_handler.setFormatter(file_formatter)
+
+# --- 2. LokiHandler (логи в Grafana Loki) ---
+loki_handler = LokiHandler(
+    url="http://localhost:3100/loki/api/v1/push",  # URL Loki
+    tags={"app": "my_app"},  # Метки для Loki 
+)
+loki_handler.setLevel(logging.INFO) 
+loki_formatter = logging.Formatter("%(message)s")  # Можно настроить иначе
+loki_handler.setFormatter(loki_formatter)
+
+# Добавляем оба обработчика к логгеру
+logger.addHandler(file_handler)
+#logger.addHandler(loki_handler)
 
 # Функция удаления логов старше 30 дней
 def delete_old_logs():
@@ -45,13 +72,13 @@ def delete_old_logs():
             f.writelines(new_logs)
 
     except Exception as e:
-        logging.error(f"Ошибка при удалении старых логов: {e}")
+        logger.error(f"Ошибка при удалении старых логов: {e}")
 
 # Функция записи логов в файл
 def log_to_file(level, function_name, context, result=None, error=None, error_code=None, traceback_info=None):
     timestamp = datetime.now(timezone.utc).isoformat()
     log_message = f"{timestamp}{log_separator}{level}{log_separator}{function_name}{log_separator}{context}{log_separator}{result if result is not None else ''}{log_separator}{error if error else ''}{log_separator}{error_code if error_code else ''}{log_separator}{traceback_info if traceback_info else ''}\n"
-    logging.info(log_message)
+    logger.info(log_message)
 
 # Декоратор логирования
 def function_log(func):
