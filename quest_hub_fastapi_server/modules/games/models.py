c4f6037189c98b59a81878cb933b329057f14cf6
Lev Kerskiy-Gosling <levker08@icloud.com>
2025-04-24 21:16:37 +0300

    ЖЕСТОЧАЙШИЙ КОМИТ С ФИКСОМ БАГА

--- quest_hub_fastapi_server/modules/games/models.py
+++ quest_hub_fastapi_server/modules/games/models.py
@@ -28,3 +28,20 @@ class Game(BaseModel):
     master_id: str
     players_id: Optional[List[str]] = None
     seed: Optional[str] = None
+
+
+class Game_Update(BaseModel):
+    """
+    Модель для обновления игры.
+    """
+    id: Optional[uuid.UUID] = None 
+    name: Optional[str] = None
+    description: Optional[str] = None
+    created_at: Optional[datetime.datetime] = None
+    is_online: Optional[bool] = None
+    place: Optional[str] = None
+    game_level: Optional[Game_Level] = None
+    number_of_players: Optional[int] = None
+    master_id: Optional[str] = None
+    players_id: Optional[List[str]] = None
+    seed: Optional[str] = None
